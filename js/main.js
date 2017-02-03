513aad320052c082eb90a2db3f47c1fc699654e0
Pushkar Kolhe <pushkarkolhe@gmail.com>
2017-02-02 20:30:38 -0500

    jquery served over https

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$('#fullpage').fullpage({
-    menu: '#menu',
-    anchors: ['home', 'reel', 'contact'],
-    sectionsColor: ['#000000', '#1BBC9B', '#7E8F7C'],
-    autoScrolling: false
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        menu: '#menu',
+        anchors: ['home', 'reel', 'contact'],
+        sectionsColor: ['#000000', '#1BBC9B', '#7E8F7C'],
+        autoScrolling: false
+    });
 });
