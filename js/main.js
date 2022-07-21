0fcc92112b3a5b96edde3d5f43544a66829887b2
Pushkar Kolhe <pkolhe@snapchat.com>
2022-07-20 17:52:37 -0700

    Adds Shorts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         menu: '#menu',
-        anchors: ['home', 'headshots', 'reel', 'contact'],
-        sectionsColor: ['#000000', '#FFFFFF', '#000000', '#000000'],
+        anchors: ['home', 'headshots', 'reel', 'shorts', 'contact'],
+        sectionsColor: ['#000000', '#FFFFFF', '#000000', '#000000', '#000000'],
         autoScrolling: false
     });
 });
